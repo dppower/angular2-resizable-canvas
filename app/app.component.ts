72925a94fab2fe0590d08ceb70c0c3afe94c6dbc
dppower <powerdp@gmail.com>
2016-12-20 18:00:39 +0000

    Initial commit.

---
+++ app/app.component.ts
@@ -0,0 +1,7 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "app-component",
+    template: `<h1>Base Angular App.</h1>`
+})
+export class AppComponent { }
---
+++ server/server.ts
@@ -0,0 +1,25 @@
+"use strict";
+
+import * as express from "express";
+import * as http from "http";
+import * as path from "path";
+import * as fs from "fs";
+
+var app = express();
+
+app.use(express.static(path.join(__dirname, "..")));
+app.use("/scripts", express.static(path.join(__dirname, "..", "..", "node_modules")));
+
+app.set("port", process.env.PORT || 3000);
+
+app.get("*", (request, response) => {
+    console.log(__dirname);
+    response.sendFile(path.join(__dirname, "..", "index.html"));
+});
+
+var server = http.createServer(app);
+
+server.listen(app.get("port"), function () {
+    console.log(`Example app is listening on port ${app.get("port")}.`);
+    console.log(`Script base url: ${__dirname}.`);
+});
